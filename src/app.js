7b2a37e48e1f92a4e2d19997e9cdc9a61b99b725
Juan Diego Cardona <juancardona1611@gmail.com>
2025-12-04 07:48:31 -0500

    fix:(correcion) corregir el app.js

--- src/app.js
+++ src/app.js
@@ -4,3 +4,12 @@ function suma(a, b) {
 
 console.log("app lista");
 console.log(suma(2, 3));
+
+function agregarItem(texto) {
+    const lista = document.getElementById('lista');
+    if (lista) {
+        const item = document.createElement('li');
+        item.textContent = texto;
+        lista.appendChild(item);
+    }
+}
